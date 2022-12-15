8974c9a65ab221c80c81d6151f05d69d5407a63e
Beko9688 <alexandrov-2021@list.ru>
2022-12-15 22:25:25 +0700

    first commit

---
+++ mini_module_app/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+import os
+
+
+class Files(models.Model):
+    subjects = (('Устройство и эволюция программного обеспечения', 'Устройство и эволюция программного обеспечения',),
+                ('Управление ИТ-проектами', 'Управление ИТ-проектами',),
+                ('Предварительная отработка данных', 'Предварительная отработка данных',),
+                ('Программирование на JavaScript', 'Программирование на JavaScript',),
+                ('Дополнительно', 'Дополнительно',))
+    types = (('Лекция', 'Лекция',), ('Практика', 'Практика',), ('Лабораторная', 'Лабораторная',))
+
+    subject = models.CharField(max_length=50, verbose_name='Предмет', choices=subjects)
+    type = models.CharField(max_length=50, verbose_name='Тип', choices=types)
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
+    file = models.FileField(verbose_name='Файл', upload_to='uploads/%Y/%m/%d/')
+
+    def __str__(self):
+        return self.title
+
+    def path(self):
+        return self.file.path
+
+    @property
+    def extension(self):
+        name, extension = os.path.splitext(self.file.name)
+        return extension
+
+    def name(self):
+        return self.subject + ' - ' + self.title
+
+    class Meta:
+        verbose_name = 'Файл'
+        verbose_name_plural = 'Файлы'
+        ordering = ['-title']
+
+
+class Calendar(models.Model):
+    title = models.CharField(max_length=10, verbose_name='Заголовок')
+    note = models.CharField(max_length=250, verbose_name='Заметка')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Заметка'
+        verbose_name_plural = 'Заметки'
+
+
+class Facts(models.Model):
+    text = models.TextField()
